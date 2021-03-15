8e2b79723f943b7c0214aeb311552a1928f04071
Adam Rodrigues <36482887+arodrigues97@users.noreply.github.com>
2021-03-15 14:48:47 -0400

    Added validation for registering

--- api/resolvers/user/userMutation.js
+++ api/resolvers/user/userMutation.js
@@ -1,9 +1,59 @@
 import { getUser, getToken } from "../../utils/auth.js"
+import { UserInputError } from "apollo-server"
 
+/**
+ * The email regex used for validation.
+ */
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+/**
+ * The method used to handle the registering of a new user.
+ * @param {*} parent The previous resolver in the chain.
+ * @param {*} args The arguments passed to the resolver.
+ * @param {*} context The context object provided.
+ * @returns The authentication token and user object.
+ */
+const register = async function (parent, args, context) {
+  let { email, firstName, lastName, password } = args
+  if (firstName.length < 1) {
+    throw new UserInputError("You must provide a first name.")
+  }
+  if (lastName.length < 1) {
+    throw new UserInputError("You must provide a last name.")
+  }
+  if (!ValidateEmail(email)) {
+    throw new UserInputError("The email you provided is not a valid email.")
+  }
+  if (password.length < 6 || password.length > 32) {
+    throw new UserInputError(
+      "A password must be between 6 and 32 characters long."
+    )
+  }
+  //TODO: complex passwords...
+  let user = await context.prisma.user.create({
+    data: {
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+      password: password,
+    },
+  })
+  const token = getToken(user.id)
+  return { token, user }
+}
+
+/**
+ * The method used to handle the registering of a new user.
+ * @param {*} parent The previous resolver in the chain.
+ * @param {*} args The arguments passed to the resolver.
+ * @param {*} context The context object provided.
+ * @returns The authentication token and user object.
+ */
 const login = async function (parent, args, context) {
   let email = args.email
   let password = args.password
   const validationErrors = {}
+
   if (email.length == 0) {
     throw new UserInputError("You must enter an email.")
   }
@@ -25,17 +75,13 @@ const login = async function (parent, args, context) {
   return { token, user }
 }
 
-const register = async function (parent, args, context) {
-  let user = await context.prisma.user.create({
-    data: {
-      email: args.email,
-      firstName: args.firstName,
-      lastName: args.lastName,
-      password: args.password,
-    },
-  })
-  const token = getToken(user.id)
-  return { token, user }
+/**
+ * Checks if the email given is valid.
+ * @param {string} email The email to validate.
+ * @returns True if valid.
+ */
+function ValidateEmail(email) {
+  return EMAIL_REGEX.test(email)
 }
 
 export default {
