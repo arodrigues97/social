eade648879865009e346c2e0983037158f62ef32
Adam Rodrigues <36482887+arodrigues97@users.noreply.github.com>
2021-03-15 21:00:26 -0400

    Fixed imports, changed some styles

--- app/src/feed/FeedPage.js
+++ app/src/feed/FeedPage.js
@@ -3,19 +3,21 @@ import {
   Container,
   LinearProgress,
   makeStyles,
+  Grid,
 } from "@material-ui/core"
 import Post from "../components/post/Post"
 import CreatePost from "../components/post/CreatePost"
 import Alert from "@material-ui/lab/Alert"
-import { useLazyQuery } from "@apollo/client"
+import { useLazyQuery, useQuery } from "@apollo/client"
 import GET_FEED from "./getFeedQuery"
+import GET_USER from "./getUserQuery"
 import { useEffect, useRef, useState } from "react"
 
 /**
  * Creates the styling of the feed page.
  */
 const useStyles = makeStyles((theme) => ({
-  container: {
+  root: {
     marginTop: theme.spacing(4),
   },
   error: {
@@ -49,6 +51,13 @@ const FeedPage = () => {
   })
 
   /**
+   * The query hook to fetch the users data.
+   */
+  const { data: getUserData } = useQuery(GET_USER, {
+    onError({ error }) {},
+  })
+
+  /**
    * The offset used for loading more feed.
    */
   const [feedOffset, setFeedOffset] = useState(0)
@@ -62,47 +71,56 @@ const FeedPage = () => {
     getFeed({ variables: { offset: feedOffset } })
   }, [feedOffset, getFeed])
 
+  if (!getUserData) {
+    return <span>Loading...</span>
+  }
   return (
-    <Container className={classes.container} maxWidth="sm">
-      <CreatePost />
-      {loading ? <LinearProgress></LinearProgress> : ""}
-      {error ? (
-        <Alert className={classes.error} severity="error">
-          {error.graphQLErrors.map(({ message }) => {
-            return <span>{message}</span>
-          })}
-          {error.networkError
-            ? "Sorry, we are having some issues fetching the news feed."
-            : ""}
-        </Alert>
-      ) : (
-        ""
-      )}
-      <ul className={classes.postList}>
-        {data
-          ? data.getFeed.map((post, index) => {
-              return (
-                <li key={post.id}>
-                  <Post
-                    id={post.id}
-                    poster={post.user.firstName + " " + post.user.lastName}
-                    post={post.post}
-                    likesCount={post.likesCount}
-                    isLiked={post.likedByUser}
-                  />
-                </li>
-              )
-            })
-          : ""}
-      </ul>
-      <div style={{ float: "left", clear: "both" }} ref={scrollRef}></div>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => setFeedOffset(feedOffset + 1)}
-      >
-        Load more
-      </Button>
+    <Container>
+      <Grid container className={classes.root} spacing={3}>
+        <Grid item xs={12} md={4}></Grid>
+        <Grid item xs={12} md={4}>
+          <CreatePost user={getUserData.getUser} />
+          {loading ? <LinearProgress></LinearProgress> : ""}
+          {error ? (
+            <Alert className={classes.error} severity="error">
+              {error.graphQLErrors.map(({ message }) => {
+                return <span>{message}</span>
+              })}
+              {error.networkError
+                ? "Sorry, we are having some issues fetching the news feed."
+                : ""}
+            </Alert>
+          ) : (
+            ""
+          )}
+          <ul className={classes.postList}>
+            {data
+              ? data.getFeed.map((post, index) => {
+                  return (
+                    <li key={post.id}>
+                      <Post
+                        id={post.id}
+                        poster={post.user.firstName + " " + post.user.lastName}
+                        post={post.post}
+                        likesCount={post.likesCount}
+                        isLiked={post.likedByUser}
+                      />
+                    </li>
+                  )
+                })
+              : ""}
+          </ul>
+          <div style={{ float: "left", clear: "both" }} ref={scrollRef}></div>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setFeedOffset(feedOffset + 1)}
+          >
+            Load more
+          </Button>
+        </Grid>
+        <Grid item xs={12} md={4}></Grid>
+      </Grid>
     </Container>
   )
 }
