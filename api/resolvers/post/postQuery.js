7d83b3b67c3d169910cbba268e40ee9d998941ea
Adam Rodrigues <36482887+arodrigues97@users.noreply.github.com>
2021-03-16 21:23:54 -0400

    Added a query to get the users posts

--- api/resolvers/post/postQuery.js
+++ api/resolvers/post/postQuery.js
@@ -8,8 +8,21 @@ const getFeed = async function (parent, args, context) {
   })
 }
 
+const getProfilePosts = async function (parent, args, context) {
+  let user = await getUser(context)
+  return await context.prisma.post.findMany({
+    skip: 0,
+    take: 5 + args.offset * 5,
+    where: {
+      userId: user.id,
+    },
+    orderBy: { id: "desc" },
+  })
+}
+
 export default {
   Query: {
     getFeed,
+    getProfilePosts,
   },
 }
